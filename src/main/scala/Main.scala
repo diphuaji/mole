2d243e1c61ee9a60b723c507d9c596ab9ad01828
Billy Xia <billy.xia@upfeat.com>
2021-10-28 21:23:21 -0600

    Bunch of improvements.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,27 +1,27 @@
-import ConnectionStatus.ConnectionStatusVal
+import MoleConfig.TunnelConfig
 import com.jcraft.jsch._
 
 import java.awt.{BorderLayout, Component}
-import java.util.{EventObject, List, Properties}
-import java.io.IOException
-import java.util
+import java.io.{IOException, File}
 import javax.swing._
-import javax.swing.JFrame
-import javax.swing.JLabel
 import javax.swing.table.{DefaultTableCellRenderer, DefaultTableModel, TableCellEditor, TableCellRenderer}
 import scala.jdk.CollectionConverters._
 import java.awt.event.{ActionEvent, ActionListener, MouseEvent}
-import javax.swing.event.{CellEditorListener, MouseInputListener}
-import java.awt.Color
-import javax.swing.BorderFactory
 import java.awt.Color
 import java.lang.Thread.{UncaughtExceptionHandler, sleep}
-import com.typesafe.config.{ConfigFactory, ConfigRenderOptions}
+import com.typesafe.config.{Config, ConfigFactory, ConfigRenderOptions}
+
 
 class SshUserInfo(val username: String, val host: String, val port: Int = 22, val password: String) extends UserInfo {
+
+  val pathToPrivateKey = {
+    val sep = File.separator
+    f"${System.getProperty("user.home")}$sep.ssh${sep}id_rsa"
+  }
+
   def getPassphrase: String = ""
 
-  def getPassword: String = "314152380"
+  def getPassword: String = ""
 
   def promptPassword(message: String): Boolean = true
 
@@ -30,11 +30,12 @@ class SshUserInfo(val username: String, val host: String, val port: Int = 22, va
   def promptYesNo(message: String): Boolean = true
 
   def showMessage(message: String): Unit = {}
+
 }
 
-case class TunnelConfig(lport: Int, rhost: String, rport: Int, timeout_seconds: Int = 1)
 
 class TunnelThread(val jsch: JSch, val sshUserInfo: SshUserInfo, val tunnelConfig: TunnelConfig, val digButton: DigButton, val table: JTable) extends Thread {
+  private val timeoutSeconds = 3
   private var _shouldStop = false
 
   def setShouldStop(value: Boolean) = this.synchronized {
@@ -43,45 +44,50 @@ class TunnelThread(val jsch: JSch, val sshUserInfo: SshUserInfo, val tunnelConfi
   }
 
   override def run(): Unit = {
-    val session = jsch.getSession(sshUserInfo.username, sshUserInfo.host, sshUserInfo.port)
-    session.setUserInfo(sshUserInfo)
-    val assinged_port = session.setPortForwardingL(tunnelConfig.lport, tunnelConfig.rhost, tunnelConfig.rport)
-    println(f"Assigning port: {$assinged_port}")
+    println("private key: " + sshUserInfo.pathToPrivateKey)
+    jsch.addIdentity(sshUserInfo.pathToPrivateKey)
+    println("private key set")
     try {
-      session.connect(tunnelConfig.timeout_seconds * 1000)
+      val session = jsch.getSession(sshUserInfo.username, sshUserInfo.host, sshUserInfo.port)
+      println("session created")
+      session.setUserInfo(sshUserInfo)
+      println(f"Assigning port: {${tunnelConfig.lport}}")
+      val assinged_port = session.setPortForwardingL(tunnelConfig.lport, tunnelConfig.rhost, tunnelConfig.rport)
+
+      println(f"Assigned port: {$assinged_port}")
+
+      session.connect(timeoutSeconds * 1000)
       digButton.connectionStatus = ConnectionStatus.CONNECTED
       digButton.setText("Disconnect")
-    } catch {
-      case e =>
-        digButton.connectionStatus = ConnectionStatus.NOT_CONNECTED
-        digButton.setText("Connect")
-    }finally {
       digButton.setEnabled(true)
       table.repaint()
-    }
-    println(f"Assigned port: {$assinged_port}")
-    while (!_shouldStop) {
-      //      println("don't stop!")
-      sleep(1000)
-    }
 
-    session.disconnect()
-    digButton.connectionStatus = ConnectionStatus.NOT_CONNECTED
-    digButton.setText("Connect")
-    digButton.setEnabled(true)
-    table.repaint()
+      while (!_shouldStop) {
+        //      println("don't stop!")
+        sleep(1000)
+      }
+
+      session.disconnect()
+      digButton.connectionStatus = ConnectionStatus.NOT_CONNECTED
+    } catch {
+      case e =>
+        println(f"soemthing wrong:${e.getMessage}")
+        JOptionPane.showMessageDialog(digButton, f"Connection error:\n${e.getMessage}")
+        digButton.connectionStatus = ConnectionStatus.NOT_CONNECTED
+    } finally {
+      println(f"finally block")
+      digButton.setEnabled(true)
+      digButton.setText("Connect")
+      table.repaint()
+    }
   }
 }
 
-class DigTunnel extends ActionListener {
-  def actionPerformed(event: ActionEvent): Unit = {
-
-  }
-}
-
-class DigButton(val lport: Int, val rhost: String, val rport: Int) extends JButton("CONNECT") {
+class DigButton(val tunnelConfig: TunnelConfig) extends JButton("Connect") {
   private var _connectionStatus = ConnectionStatus.NOT_CONNECTED
+
   def connectionStatus = _connectionStatus
+
   def connectionStatus_=(value: ConnectionStatus.ConnectionStatusVal) = this.synchronized {
     _connectionStatus = value
   }
@@ -103,10 +109,8 @@ object ConnectionStatus extends Enumeration {
   val DISCONNECTING = ConnectionStatusVal("Disconnecting", Some(Color.YELLOW))
 }
 
-object MoleConfig {
-  case class TunnelConfig(name: String, sshHost: String, lport: Int, rhost: String, rport: Int)
-
-  private val moleConfig = ConfigFactory.load().getConfig("com.diphuaji.mole")
+class MoleConfig(val config: Config) {
+  private val moleConfig = config.getConfig("com.diphuaji.mole")
   val tunnels: Seq[TunnelConfig] = moleConfig.getConfigList("tunnels").asScala.toSeq.map { tunnel =>
     TunnelConfig(
       tunnel.getString("name"),
@@ -118,6 +122,13 @@ object MoleConfig {
   }
 
   def printConfig: Unit = println(moleConfig.root().render(ConfigRenderOptions.defaults().setOriginComments(false)))
+
+}
+
+object MoleConfig {
+  case class TunnelConfig(name: String, sshHost: String, lport: Int, rhost: String, rport: Int)
+
+  def apply(file: File) = new MoleConfig(ConfigFactory.parseFile(file))
 }
 
 object Main extends App {
@@ -128,39 +139,38 @@ object Main extends App {
   val ACTION_COL = 5
 
   //Create and set up the window.
-  val frame: JFrame = new JFrame("HelloWorldSwing")
+  val frame: JFrame = new JFrame("Mole")
   //  frame.setLayout()
   frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
 
-
   val tableModel = new DefaultTableModel()
-  tableModel.addColumn("lport")
-  tableModel.addColumn("rhost")
-  tableModel.addColumn("rport")
-  tableModel.addColumn("server")
-  tableModel.addColumn("status")
-  tableModel.addColumn("action")
+  val table = new JTable(tableModel)
 
-  val rows = MoleConfig.tunnels.map { tunnel =>
-    Seq(tunnel.lport, tunnel.rhost, tunnel.rport, tunnel.sshHost)
-  }
+  def setupUI(tunnels: Seq[TunnelConfig]) = {
 
-  val actionButtons = rows.zipWithIndex.map({
-    case (row, index) =>
-      val button = new DigButton(row.head.asInstanceOf[Int], row(1).asInstanceOf[String], row(2).asInstanceOf[Int])
+    tableModel.addColumn("lport")
+    tableModel.addColumn("rhost")
+    tableModel.addColumn("rport")
+    tableModel.addColumn("server")
+    tableModel.addColumn("status")
+    tableModel.addColumn("action")
+
+    tunnels.foreach(tunnel =>
+      tableModel.addRow(Seq(tunnel.lport, tunnel.rhost, tunnel.rport, tunnel.sshHost).asJava.toArray())
+    )
+    val actionButtons = tunnels.map { tunnel =>
+      val button = new DigButton(tunnel)
       button.addActionListener(new ActionListener {
         def actionPerformed(event: ActionEvent) = {
-          val sshUserInfo = new SshUserInfo(username = "billy", host = "localhost", password = "314152380")
-          val tunnelConfig = TunnelConfig(button.lport, button.rhost, button.rport)
-
+          val sshUserInfo = new SshUserInfo(username = System.getProperty("user.name"), host = tunnel.sshHost, password = "314152380")
           button.connectionStatus match {
             case ConnectionStatus.NOT_CONNECTED =>
-              val thread = new TunnelThread(jsch, sshUserInfo, tunnelConfig, button, table)
+              val thread = new TunnelThread(jsch, sshUserInfo, tunnel, button, table)
               button.tunnelThread = Some(thread)
               thread.setUncaughtExceptionHandler(new UncaughtExceptionHandler {
                 def uncaughtException(var1: Thread, var2: Throwable) = {
                   //                  println("something wrong!")
-                  button.setText("CONNECT")
+                  button.setText("Connect")
                 }
               })
               thread.start()
@@ -177,52 +187,66 @@ object Main extends App {
         }
       })
       button
-  })
-  rows.foreach(row => tableModel.addRow(row.asJava.toArray()))
-
-  val table = new JTable(tableModel)
-
-  val actionColumn = table.getColumnModel.getColumn(ACTION_COL)
-  actionColumn.setPreferredWidth(100)
-  actionColumn.setCellRenderer(new DefaultTableCellRenderer {
-    override def getTableCellRendererComponent(table: JTable, value: Any, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int) = {
-      actionButtons(row)
-    }
-  })
-
-  actionColumn.setCellEditor(new AbstractCellEditor with TableCellEditor {
-    def getTableCellEditorComponent(table: JTable, value: Any, isSelected: Boolean, row: Int, column: Int) = {
-      actionButtons(row)
     }
 
-    def getCellEditorValue = {
-      1
-    }
-  })
+    val actionColumn = table.getColumnModel.getColumn(ACTION_COL)
+    actionColumn.setPreferredWidth(100)
+    actionColumn.setCellRenderer(new DefaultTableCellRenderer {
+      override def getTableCellRendererComponent(table: JTable, value: Any, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int) = {
+        actionButtons(row)
+      }
+    })
 
-  val statusColumn = table.getColumnModel.getColumn(STATUS_COL)
-  statusColumn.setCellRenderer(new DefaultTableCellRenderer {
-    override def getTableCellRendererComponent(table: JTable, value: Any, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int) = {
-      actionButtons(row).connectionStatus.color.map(color => {
-        //        println(color)
-        val l = new JLabel()
-        l.setOpaque(true)
-        l.setBackground(color)
-        l.setForeground(Color.cyan)
-        l
-      }).getOrElse(new JLabel())
-    }
-  })
+    actionColumn.setCellEditor(new AbstractCellEditor with TableCellEditor {
+      def getTableCellEditorComponent(table: JTable, value: Any, isSelected: Boolean, row: Int, column: Int) = {
+        actionButtons(row)
+      }
 
-  //Add the ubiquitous "Hello World" label.
-  val label: JLabel = new JLabel("Hello World")
-  val footer: JButton = new JButton("Binggo")
-  footer.getPressedIcon
+      def getCellEditorValue = {
+        1
+      }
+    })
+
+    val statusColumn = table.getColumnModel.getColumn(STATUS_COL)
+    statusColumn.setCellRenderer(new DefaultTableCellRenderer {
+      override def getTableCellRendererComponent(table: JTable, value: Any, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int) = {
+        actionButtons(row).connectionStatus.color.map(color => {
+          //        println(color)
+          val l = new JLabel()
+          l.setOpaque(true)
+          l.setBackground(color)
+          l.setForeground(Color.cyan)
+          l
+        }).getOrElse(new JLabel())
+      }
+    })
+  }
+
+  val footer: JLabel = new JLabel("From Diphuaji with Love")
   val scrollPane = new JScrollPane(table)
   table.setFillsViewportHeight(true)
   val contentPane = frame.getContentPane
+
+  val fc = new JFileChooser()
+  val openFileButton = new JButton("Open Config")
+  openFileButton.addActionListener(new ActionListener {
+    override def actionPerformed(actionEvent: ActionEvent): Unit = {
+      try {
+        val returnVal = fc.showOpenDialog(openFileButton)
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+          val file = fc.getSelectedFile
+          val moleConfig = MoleConfig.apply(file)
+          setupUI(moleConfig.tunnels)
+          openFileButton.setEnabled(false)
+        }
+      } catch {
+        case e: Throwable =>
+          JOptionPane.showMessageDialog(openFileButton, f"The config file is invalid:\n${e.getMessage}")
+      }
+    }
+  })
   contentPane.add(scrollPane, BorderLayout.CENTER)
-  contentPane.add(label, BorderLayout.PAGE_START)
+  contentPane.add(openFileButton, BorderLayout.PAGE_START)
   contentPane.add(footer, BorderLayout.PAGE_END)
 
   scrollPane.add(new JLabel("xxx"))
